fa824b29910372fe3aec35f3fb7708ae156ce119
Jang Ji Woong <wkdwldnd200487@gmail.com>
2025-05-05 19:03:48 +0900

    소스코드 수정

--- filetime.c
+++ filetime.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "filetime.h"
 
+const char *file1 = "./text1";
+const char *file2 = "./text2";
+
 void filetime1() {
+    if (stat(file1, &stat1) != 0) {
+        perror("no file for text1");
+        return;
+    }
+    
     struct tm *temp = localtime(&stat1.st_mtime);
     if (temp != NULL) {
         time1_val = *temp;
@@ -11,6 +19,13 @@ void filetime1() {
 }
 
 void filetime2() {
+
+    // // Get file state
+    if (stat(file2, &stat2) != 0) {
+        perror("no file for text2");
+        return;
+    }
+
     struct tm *temp = localtime(&stat2.st_mtime);
     if (temp != NULL) {
         time2_val = *temp;
