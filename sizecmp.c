9be1689658a3e487af13430c5eea0db1d5b8774f
Jang Ji Woong <wkdwldnd200487@gmail.com>
2025-05-05 19:04:58 +0900

    file1, file2 stat 추가

--- sizecmp.c
+++ sizecmp.c
@@ -1,7 +1,17 @@
-#include <stdio.h>
 #include "sizecmp.h"
 
 void sizecmp() {
+    if (stat(file1, &stat1) != 0) {
+        perror("no file");
+        return;
+    }
+
+    // // Get file state
+    if (stat(file2, &stat2) != 0) {
+        perror("no file");
+        return;
+    }
+
     printf("size compare\n");
     if (stat1.st_size > stat2.st_size)
         printf("text1 is bigger\n");
