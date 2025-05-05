65662fdfd9a135f504a318c7f08b6893d2c2be87
Thevenin1849 <sysy1849@gmail.com>
2025-05-05 18:10:37 +0900

    ".txt"  >> " " 파일명 수정정

--- filestat.c
+++ filestat.c
@@ -5,13 +5,13 @@
 struct stat stat1, stat2;
 
 void filestat1() {
-    if(stat("text1.txt", &stat1) == -1) {
+    if(stat("text1", &stat1) == -1) {
         perror("stat error for text1.txt");
     }
 }
 
 void filestat2() {
-    if(stat("text2.txt", &stat2) == -1) {
+    if(stat("text2", &stat2) == -1) {
         perror("stat error for text2.txt");
     }
 }
