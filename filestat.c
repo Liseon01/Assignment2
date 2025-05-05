852ef394fe9028da7206e07e66d2b24bf2394ee6
Thevenin1849 <sysy1849@gmail.com>
2025-05-05 18:13:41 +0900

    출력 메시지 추가

--- filestat.c
+++ filestat.c
@@ -5,13 +5,17 @@
 struct stat stat1, stat2;
 
 void filestat1() {
-    if(stat("text1", &stat1) == -1) {
-        perror("stat error for text1.txt");
+    if (stat("text1", &stat1) == -1) {
+        perror("stat error for text1");
+    } else {
+        printf("Successfully loaded file information for text1.\n");
     }
 }
 
 void filestat2() {
-    if(stat("text2", &stat2) == -1) {
-        perror("stat error for text2.txt");
+    if (stat("text2", &stat2) == -1) {
+        perror("stat error for text2");
+    } else {
+        printf("Successfully loaded file information for text2.\n");
     }
 }
