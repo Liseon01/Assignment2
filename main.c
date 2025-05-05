29c9f0ae2a06eaa3cdbad5d53c4abdd994121cd4
Thevenin1849 <sysy1849@gmail.com>
2025-05-05 20:16:05 +0900

    Main 함수 수정정

--- main.c
+++ main.c
@@ -16,16 +16,16 @@ int main() {
     struct stat stat1, stat2;
     time_t time1, time2;
 
-    filestat(file1, &stat1);
-    filestat(file2, &stat2);
+    filestat1(&stat1);
+    filestat2(&stat2);
 
-    filetime(file1, &time1);
-    filetime(file2, &time2);
+    filetime1(file1, &time1);
+    filetime2(file2, &time2);
 
     sizecmp(file1, file2);
     blockcmp(file1, file2);
     datecmp(file1, file2);
-    timecmp(file1, &time1, file2, &time2);
+    timecmp(file1, time1, file2, time2);
 
     return 0;
 }
