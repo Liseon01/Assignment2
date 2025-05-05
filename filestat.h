7627a718777b0350ca3889118c765a993dfeba9e
Thevenin1849 <sysy1849@gmail.com>
2025-05-05 20:16:40 +0900

    filestat함수 수정

--- filestat.h
+++ filestat.h
@@ -1,12 +1,9 @@
 #ifndef FILESTAT_H
 #define FILESTAT_H
 
-#include <sys/types.h>
 #include <sys/stat.h>
 
-extern struct stat stat1, stat2;
-
-void filestat1();
-void filestat2();
+void filestat1(struct stat *statbuf);
+void filestat2(struct stat *statbuf);
 
 #endif
