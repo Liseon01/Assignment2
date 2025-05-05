33bcc839c9daf7ab237f848c09d687e071bb33df
Sungjun24s <105345819+Sungjun24s@users.noreply.github.com>
2025-05-05 18:01:45 +0900

    datecmp.c 오타 수정

--- datecmp.c
+++ datecmp.c
@@ -1,29 +1,36 @@
-#include <datecmp.h>>
+#include <datecmp.h>
 
-void datecmp(){
+void datecmp()
+{
 
     // Get file state
-    if (stat(file1, &stat1) != 0) {
+    if (stat(file1, &stat1) != 0)
+    {
         perror("no file");
         return;
     }
 
     // // Get file state
-    if (stat(file2, &stat2) != 0) {
+    if (stat(file2, &stat2) != 0)
+    {
         perror("no file");
         return;
     }
 
-    
     struct tm time1_cpy = *localtime(&stat1.st_mtime);
     struct tm time2_cpy = *localtime(&stat2.st_mtime);
     printf("date compare\n");
     // Compare month & date
-    if (time1_cpy.tm_mon+1 > time2_cpy.tm_mon+1 || ((time1_cpy.tm_mon+1 == time2_cpy.tm_mon+1) && (time1_cpy.tm_mday > time2_cpy.tm_mday))) {
+    if (time1_cpy.tm_mon + 1 > time2_cpy.tm_mon + 1 || ((time1_cpy.tm_mon + 1 == time2_cpy.tm_mon + 1) && (time1_cpy.tm_mday > time2_cpy.tm_mday)))
+    {
         printf("Text2 is early\n\n");
-    } else if (time1_cpy.tm_mon+1 < time2_cpy.tm_mon+1 || ((time1_cpy.tm_mon+1 == time2_cpy.tm_mon+1) && (time1_cpy.tm_mday < time2_cpy.tm_mday))) {
+    }
+    else if (time1_cpy.tm_mon + 1 < time2_cpy.tm_mon + 1 || ((time1_cpy.tm_mon + 1 == time2_cpy.tm_mon + 1) && (time1_cpy.tm_mday < time2_cpy.tm_mday)))
+    {
         printf("Text1 is early\n\n");
-    } else {
+    }
+    else
+    {
         printf("same time\n\n");
     }
 }
